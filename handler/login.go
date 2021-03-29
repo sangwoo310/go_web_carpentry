8209a6bc5d84dba1516e736e86a37ceed5c1b24e
hansangwoo <sangwoo310@naver.com>
2021-03-30 00:22:27 +0900

    c.file 을 통한 페이지 연결

--- handler/login.go
+++ handler/login.go
@@ -2,17 +2,29 @@ package handler
 
 import (
 	"github.com/labstack/echo"
+	"log"
 	"net/http"
 )
 
 //Login
 func Login(c echo.Context) error {
-	if err:= c.String(http.StatusOK, "loginCheck!"); err != nil {
+	if err := c.String(http.StatusOK, "loginCheck!"); err != nil {
 		return err
 	}
 	return nil
 }
 
-func Landering(c echo.Context) error {
-	c.Render()
+func Rendering(c echo.Context) error {
+	log.Println("start rendering")
+	//c.SetRequest()
+
+	if err := c.Render(http.StatusOK, "views/index.html", nil); err != nil {
+		return err
+	}
+
+	//if err := c.File("views/index.html"); err != nil {
+	//	return err
+	//}
+
+	return nil
 }
